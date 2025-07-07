d45fb41e13536089405d75a157c0c56fd6a2200c
Brayan Pantoja <pantojab125.com>
2025-07-07 13:13:40 -0500

    Ajuste de lavista dellistado de comunas

--- app/Http/Controllers/ComunaController.php
+++ app/Http/Controllers/ComunaController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Comuna;
+use App\Models\Municipio;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -16,7 +17,11 @@ class ComunaController extends Controller
      */
     public function index()
     {
-        $comunas = Comuna::all();
+        // $comunas = Comuna::all();
+        $comunas = DB::table('tb_comuna')
+        ->join('tb_municipio', 'tb_comuna.muni_codi', '=', 'tb_municipio.muni_codi')
+        ->select('tb_comuna.*', "tb_municipio.muni_nomb")
+        ->get();
         return view('comuna.index', ['comunas' => $comunas]);
     }
 
